1abedea652b395acc5a78c5df809cc574a626612
Halomoancarel1 <37604253+Halomoancarel1@users.noreply.github.com>
2020-04-28 19:47:55 +0700

    new API

---
+++ rest-api/Controllers/ValuesController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using rest_api.Models.DBModels;
+
+namespace rest_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private sekolahContext _context;
+
+        public ValuesController(sekolahContext context)
+        {
+            this._context = context;
+        }
+
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var students = _context.Siswa.ToList();
+            return Ok(students);
+            //return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            var studentById = (from a in _context.Siswa
+                               where a.Id == id
+                               select new Siswa
+                               {
+                                   Nama = a.Nama,
+                                   Alamat = a.Alamat
+                               }).FirstOrDefault();
+            return Ok(studentById);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ rest-api/Models/DBModels/sekolahContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace rest_api.Models.DBModels
+{
+    public partial class sekolahContext : DbContext
+    {
+        public sekolahContext()
+        {
+        }
+
+        public sekolahContext(DbContextOptions<sekolahContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Siswa> Siswa { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySql("Server=localhost;Database=sekolah;User=root;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Siswa>(entity =>
+            {
+                entity.ToTable("siswa");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.Alamat)
+                    .IsRequired()
+                    .HasColumnName("alamat")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Nama)
+                    .IsRequired()
+                    .HasColumnName("nama")
+                    .HasColumnType("varchar(25)");
+
+                entity.Property(e => e.created_date)
+                    .IsRequired()
+                    .HasColumnName("created_date")
+                    .HasColumnType("datetime");
+            });
+        }
+    }
+}
